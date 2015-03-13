01bf0c38d3d6eaa9f51e602dfc90184fa76d73b3
ximelainfiesta <ximena.lainfiesta@gmail.com>
2015-03-13 16:59:34 -0600

    Codeacademy battleship, plus a board 10 x 10

--- application.py
+++ application.py
@@ -1,3 +1,45 @@
-# Aqui escribe tu codigo
+import random
 
-print "Hello World"
+tablero = []
+
+for x in range(0,10):
+  tablero.append(["O"] * 10)
+
+def print_tablero(tablero):
+  for fila in tablero:
+    print " ".join(fila)
+
+print "Juguemos batalla naval!"
+print_tablero(tablero)
+
+def fila_aleatoria(tablero):
+  return random.randint(0,len(tablero)-1)
+
+def columna_aleatoria(tablero):
+  return random.randint(0,len(tablero[0])-1)
+
+barco_fila = fila_aleatoria(tablero)
+barco_col = columna_aleatoria(tablero)
+print barco_fila
+print barco_col
+
+for turno in range(4):
+    adivina_fila = input("Adivina fila:")
+    adivina_columna = input("Adivina columna:")
+
+    if adivina_fila == barco_fila and adivina_columna == barco_col:
+        print "Felicitaciones! Hundiste mi barco!"
+        break
+    else:
+        if (adivina_fila < 0 or adivina_fila > 5) or (adivina_columna < 0 or adivina_columna > 5):
+            print "Vaya, esto ni siquiera esta en el oceano."
+        elif(tablero[adivina_fila-1][adivina_columna-1] == "X"):
+            print "Ya dijiste esa."
+        else:
+          print "No impactaste mi barco!"
+          tablero[adivina_fila-1][adivina_columna-1] = "X"
+        if turno == 3:
+            print "Termino el juego"
+
+    print turno + 1
+    print_tablero(tablero)
