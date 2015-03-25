a64f27f17984fbbd0475f0d81e67b9046d4e228d
ximelainfiesta <ximena.lainfiesta@gmail.com>
2015-03-25 16:28:42 -0600

    Validation fixed

--- application.py
+++ application.py
@@ -35,90 +35,46 @@ class Game(object):
     def columna_aleatoria(self):
         return random.randint(0,len(self.board[0])-1)
 
-
-
-    def validar_horizontal(self, barco_fila, barco_col, contador):
-        """Validacion Posicion Horizontal"""
-        cont_barcos = 0
-        for i in range(0, contador):
-            #print  "Contador: " + str(i)
-            if self.board[barco_fila + i][barco_col] == "⚓":
-                #print False
-                cont_barcos += 1
-            else:
-                #print True
-                cont_barcos += 0
-
-        if cont_barcos == 0:
-            barco_fila = 0
-            barco_col = 0
-            return 1
-        else:
-            barco_fila = 0
-            barco_col = 0
-            return 2
-
-    def validar_vertical(self, barco_fila, barco_col, contador):
-        """Validacion posicion Vertical"""
-        cont_barcos = 0
-        for i in range(0, contador):
-            #print  "Contador: " + str(i)
-            if self.board[barco_fila][barco_col + i] == "⚓":
-                #print False
-                cont_barcos += 1
-            else:
-                #print True
-                cont_barcos += 0
-
-        if cont_barcos == 0:
-            barco_fila = 0
-            barco_col = 0
-            return 1
-        else:
-            barco_fila = 0
-            barco_col = 0
-            return 2
-
-
-
-
-
     def ship_variables(self):
         self.barco_fila = self.fila_aleatoria()
         self.barco_col = self.columna_aleatoria()
-        self.barco_fil = self.fila_aleatoria()
-        self.barco_co = self.columna_aleatoria()
-
-
 
         print self.barco_fila
         print self.barco_col
-        print self.barco_fil
-        print self.barco_co
-
-
 
     def user_entry(self):
-        for turno in range(4):
-            adivina_fila = input("Adivina fila:")
-            adivina_columna = input("Adivina columna:")
+        turno = 0
+        while turno < 8:
+            turno = turno + 1
+            print "Turno: ", turno
+            adivina_fila = raw_input("Adivina fila: ")
+            adivina_columna = raw_input("Adivina columna:")
+            try:
+                adivina_fila = int(adivina_fila)
+                adivina_columna = int(adivina_columna)
 
-            if adivina_fila == self.barco_fila and adivina_columna == self.barco_col or adivina_fila == self.barco_fil and adivina_columna == self.barco_co:
-                print "Felicitaciones! Hundiste mi barco!"
-                break
-            else:
-                if (adivina_fila < 0 or adivina_fila > 11) or (adivina_columna < 0 or adivina_columna > 11):
-                    print "Vaya, esto ni siquiera esta en el oceano."
-                elif(self.board[adivina_fila-1][adivina_columna-1] == "X"):
-                    print "Ya dijiste esa."
+                if adivina_fila == self.barco_fila and adivina_columna == self.barco_col:
+                    print "Felicitaciones! Hundiste mi barco!"
+                    break
                 else:
-                  print "No impactaste mi barco!"
-                  self.board[adivina_fila-1][adivina_columna-1] = "X"
-                if turno == 3:
-                    print "Termino el juego"
+                    if (adivina_fila < 0 or adivina_fila >= 10) or (adivina_columna < 0 or adivina_columna >= 10):
+                        print "Vaya, esto ni siquiera esta en el oceano."
+                    elif(self.board[adivina_fila][adivina_columna] == "X"):
+                        print "Ya dijiste esa."
+                    else:
+                      print "No impactaste mi barco!"
+                      self.board[adivina_fila][adivina_columna] = "X"
+                    if turno == 8:
+                        print "Termino el juego"
 
-            print turno + 1
-            self.print_board()
+                self.print_board()
+            except (ValueError, SyntaxError):
+                print "Ingrese solo números"
+
+
+
+
+
 
     def call(self):
         self.fill_board()
