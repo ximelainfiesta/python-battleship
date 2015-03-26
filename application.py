ed7aefa470cefa2cceafdd44d13c7d3575d4a12b
ximelainfiesta <ximena.lainfiesta@gmail.com>
2015-03-26 15:55:53 -0600

    everything in english and nosetest

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+""" BATTLESHIP """
 import random
 import sys
 
@@ -7,13 +8,15 @@ class Game(object):
     def __init__(self):
         """Single Player Battleship"""
         self.board = [] #empty list that will contain the board
+        self.ships = {"Carrier":5, "Battleship":4, "Cruiser":3, "Submarine":3, "Destroyer":2}
 
     def fill_board(self):
-        for x in range(0,10): #for each element 
-          self.board.append([" "] * 10) #multiply it 10 times to create board
+        """Fills board with numbers 0-9"""
+        for i in range(0, 10): #for each element
+            self.board.append([" "] * 10) #multiply it 10 times to create board
 
     def print_board(self):
-        """function to print board"""
+        """Prints board"""
         print """
                       Let's play Battleship!
         Instructions: Guess the Row and the Column of the ship. You have 5 turns
@@ -28,72 +31,77 @@ class Game(object):
             cont += 1
 
     def position(self):
-        return random.randit(1,2)
+        """Generates a random position to determine H or V direction"""
+        return random.randit(1, 2)
 
-    def fila_aleatoria(self):
-        return random.randint(0,len(self.board)-1)
+    def row_random(self):
+        """Generates a random row number"""
+        return random.randint(0, len(self.board)-1)
 
-    def columna_aleatoria(self):
-        return random.randint(0,len(self.board[0])-1)
+    def column_random(self):
+        """Generates a random column number"""
+        return random.randint(0, len(self.board[0])-1)
 
     def ship_variables(self):
-        self.barco_fila = self.fila_aleatoria()
-        self.barco_col = self.columna_aleatoria()
-
-        print self.barco_fila
-        print self.barco_col
+        """ Calls my functions above in a variable """
+        self.ship_row = self.row_random() #this should be defined in init
+        self.ship_col = self.column_random()
+        print self.ship_row
+        print self.ship_col
 
     def user_entry(self):
-        turno = 0
-        while turno < 5:
-            turno = turno + 1
-            print "Turno: ", turno
-            adivina_fila = raw_input("Adivina fila: ")
-            adivina_columna = raw_input("Adivina columna:")
+        turn = 0
+        while turn < 5:
+            turn = turn + 1
+            print "Turn: ", turn
+            guess_row = raw_input("Guess Row: ")
+            guess_column = raw_input("Guess Column:")
             try:
-                adivina_fila = int(adivina_fila)
-                adivina_columna = int(adivina_columna)
+                guess_row = int(guess_row)
+                guess_column = int(guess_column)
 
-                if adivina_fila == self.barco_fila and adivina_columna == self.barco_col:
-                    print "Felicitaciones! Hundiste mi barco!"
+                if guess_row == self.ship_row and guess_column == self.ship_col:
+                    print "Congratulations, you sank my ship!"
                     self.play_again()
                 else:
-                    if (adivina_fila < 0 or adivina_fila >= 10) or (adivina_columna < 0 or adivina_columna >= 10):
-                        print "Vaya, esto ni siquiera esta en el oceano."
-                    elif(self.board[adivina_fila][adivina_columna] == "X"):
-                        print "Ya dijiste esa."
+                    if (guess_row < 0 or guess_row >= 10)or(guess_column < 0 or guess_column >= 10):
+                        print "Wow, this is not even in the ocean."
+                    elif self.board[guess_row][guess_column] == "X":
+                        print "You already said that."
                     else:
-                      print "No impactaste mi barco!"
-                      self.board[adivina_fila][adivina_columna] = "X"
-                    if turno == 5:
-                        print "Terminó el juego"
+                        print "You did not hit my ship!"
+                        self.board[guess_row][guess_column] = "X"
+                    if turn == 5:
+                        print "Game Over"
                         self.play_again()
-                        
+
                 self.print_board()
             except (ValueError, SyntaxError):
-                print "Ingrese solo números"
+                print "Enter only numbers"
 
     def play_again(self):
+        """ Option to repeat the game """
         while True:
             try:
-                user = raw_input("Quieres jugar de nuevo? Y/N: ")
+                user = raw_input("Do you want to play again? Y/N: ")
                 user = user.lower()
                 if user == "y":
                     self.board = []
                     self.call()
                 elif user == "n":
-                    print "Adiós!!"
+                    print "Good-Bye!!"
                     sys.exit()
                 else:
-                    print "Solo ingrese Y o N"
+                    print "Only enter Y o N"
             except TypeError:
-                print "Opción inválida"
+                print "Invalid Option"
 
     def call(self):
+        """ Calls the functions in order """
         self.fill_board()
         self.print_board()
-        self.fila_aleatoria()
-        self.columna_aleatoria()
+        self.row_random()
+        self.column_random()
         self.ship_variables()
         self.user_entry()
 
